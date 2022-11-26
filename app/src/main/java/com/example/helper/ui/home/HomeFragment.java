cebb7424713947464eb062fab2b082f2447b6a6d
Senpai0210 <dimakonohov2003@gmail.com>
2022-11-26 16:18:51 +0300

    Helper

--- app/src/main/java/com/example/helper/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/helper/ui/home/HomeFragment.java
@@ -1,20 +1,14 @@
 package com.example.helper.ui.home;
 
-import androidx.lifecycle.ViewModelProvider;
-
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-import androidx.navigation.NavController;
-import androidx.navigation.fragment.NavHostFragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.example.helper.MainActivity;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
 import com.example.helper.R;
 import com.example.helper.viewmodels.HomeViewModel;
 
