4a93be6b540b505dfa9a4983461ff0c965a14811
Senpai0210 <dimakonohov2003@gmail.com>
2022-11-26 16:03:27 +0300

    Helper

---
+++ app/src/main/java/com/example/helper/ui/home/HomeFragment.java
@@ -0,0 +1,44 @@
+package com.example.helper.ui.home;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.helper.MainActivity;
+import com.example.helper.R;
+import com.example.helper.viewmodels.HomeViewModel;
+
+public class HomeFragment extends Fragment {
+
+    private HomeViewModel mViewModel;
+//    private NavController navController = NavHostFragment.findNavController(this);
+
+    public static HomeFragment newInstance() {
+        return new HomeFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+//        ((MainActivity) getActivity()).setTitle("Главная");
+//        ((MainActivity) getActivity()).setRightIcon(R.drawable.ic_add);
+//        ((MainActivity) getActivity()).rightIcon.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                navController.findDestination(R.id.action_homeFragment_to_authFragment);
+//            }
+//        });
+        return inflater.inflate(R.layout.fragment_home, container,false);
+    }
+
+}
---
+++ app/src/main/java/com/example/helper/viewmodels/AuthViewModel.java
@@ -0,0 +1,7 @@
+package com.example.helper.viewmodels;
+
+import androidx.lifecycle.ViewModel;
+
+public class AuthViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
