80dee2a36e5965c52b07c4da2db3445bb0992163
Senpai0210 <dimakonohov2003@gmail.com>
2022-12-14 14:50:06 +0300

    Add Toolbar and ButtonNavigationView

--- app/src/main/java/com/example/helper/ui/MainActivity.java
+++ app/src/main/java/com/example/helper/ui/MainActivity.java
@@ -2,13 +2,8 @@ package com.example.helper.ui;
 
 
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
